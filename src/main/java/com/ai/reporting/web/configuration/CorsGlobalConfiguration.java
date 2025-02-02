e509bdb4cc5d0d5cef669f90431a832b3303ccc9
Ilia Zakharchenia <iliazakh97@gmail.com>
2025-02-02 12:34:52 +0100

    Add base beans and settings

---
+++ src/main/java/com/ai/reporting/web/configuration/CorsGlobalConfiguration.java
@@ -0,0 +1,26 @@
+package com.ai.reporting.web.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsGlobalConfiguration implements WebFluxConfigurer {
+    private final String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins(this.allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .maxAge(600);
+    }
+
+    public CorsGlobalConfiguration(
+            @Value("${app.cors.allowed_origins}") String allowedOriginsString) {
+        this.allowedOrigins = allowedOriginsString.split(",");
+    }
+}
